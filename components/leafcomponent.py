25b8e2aa361f2a3e345b182bb3052188b0d1e58f
Ben Singleton <bhsingleton@gmail.com>
2024-08-20 16:11:57 -0400

    Implementing build logic for leaf components.

--- components/leafcomponent.py
+++ components/leafcomponent.py
@@ -1,4 +1,4 @@
-from enum import IntEnum
+from maya.api import OpenMaya as om
 from mpy import mpynode, mpyattribute
 from . import basecomponent
 
@@ -29,11 +29,77 @@ class LeafComponent(basecomponent.BaseComponent):
 
         # Edit skeleton specs
         #
-        rootSpec, = self.resizeSkeletonSpecs(1, skeletonSpecs)
-        rootSpec['name'] = self.formatName()
-        rootSpec['driver'] = self.formatName(type='control')
+        leafSpec, = self.resizeSkeletonSpecs(1, skeletonSpecs)
+        leafSpec.name = self.formatName()
+        leafSpec.driver = self.formatName(type='control')
 
         # Call parent method
         #
         super(LeafComponent, self).invalidateSkeletonSpecs(skeletonSpecs)
+
+    def buildSkeleton(self):
+        """
+        Builds the skeleton for this component.
+
+        :rtype: Tuple[mpynode.MPyNode]
+        """
+
+        # Get skeleton specs
+        #
+        leafSpec, = self.skeletonSpecs()
+
+        # Create joint
+        #
+        leafJoint = self.scene.createNode('joint', name=leafSpec.name)
+        leafJoint.side = self.componentSide
+        leafJoint.type = self.Type.OTHER
+        leafJoint.otherType = self.componentName
+        leafJoint.displayLocalAxis = True
+        leafSpec.uuid = leafJoint.uuid()
+
+        # Update joint transform
+        #
+        leafMatrix = leafSpec.getMatrix(default=om.MMatrix.kIdentity)
+        leafJoint.setWorldMatrix(leafMatrix)
+
+        return (leafJoint,)
+
+    def buildRig(self):
+        """
+        Builds the control rig for this component.
+
+        :rtype: None
+        """
+
+        # Decompose component
+        #
+        controlsGroup = self.scene(self.controlsGroup)
+        privateGroup = self.scene(self.privateGroup)
+        jointsGroup = self.scene(self.jointsGroup)
+
+        leafSpec, = self.skeletonSpecs()
+        leafExportJoint = self.scene(leafSpec.uuid)
+
+        componentSide = self.Side(self.componentSide)
+        rigScale = self.findControlRig().getRigScale()
+
+        parentExportJoint, parentExportCtrl = self.getAttachmentTargets()
+
+        # Create control
+        #
+        leafSpaceName = self.formatName(type='space')
+        leafSpace = self.scene.createNode('transform', name=leafSpaceName, parent=controlsGroup)
+        leafSpace.copyTransform(leafExportJoint)
+        leafSpace.freezeTransform()
+
+        leafSpace.addConstraint('transformConstraint', [parentExportCtrl], maintainOffset=True)
+
+        leafCtrlName = self.formatName(type='control')
+        leafCtrl = self.scene.createNode('transform', name=leafCtrlName, parent=leafSpace)
+        leafCtrl.addPointHelper('disc', size=(10.0 * rigScale), side=componentSide)
+        leafCtrl.prepareChannelBoxForAnimation()
+        leafCtrl.tagAsController()
+        self.publishNode(leafCtrl, alias=self.componentName)
+
+        leafCtrl.userProperties['space'] = leafSpace.uuid()
     # endregion
