e4c4e42329f8322ada4768550cafee7b0fe4c275
Ben Singleton <bhsingleton@gmail.com>
2025-01-21 08:00:53 -0500

    Reworking limb overrides following changes made to limb component.

--- components/extremitycomponent.py
+++ components/extremitycomponent.py
@@ -35,15 +35,33 @@ class ExtremityComponent(basecomponent.BaseComponent):
     # endregion
 
     # region Methods
-    @abstractmethod
-    def locomotionType(self):
+    def effectorMatrix(self):
         """
-        Returns the locomotion type for this component.
+        Returns the effector matrix for this component.
 
-        :rtype: LocomotionType
+        :rtype: om.MMatrix
         """
 
-        return self.LocomotionType.NONE
+        skeletonSpecs = self.skeletonSpecs()
+        hasSkeletonSpecs = len(skeletonSpecs) > 0
+
+        if hasSkeletonSpecs:
+
+            return skeletonSpecs[0].worldMatrix
+
+        else:
+
+            return om.MMatrix.kIdentity
+
+    def preferredEffectorMatrix(self):
+        """
+        Returns the preferred effector matrix for this component.
+        By default, this will return the first skeletal spec matrix!
+
+        :rtype: om.MMatrix
+        """
+
+        return self.effectorMatrix()
 
     def overrideLimbPoleVector(self, extremityCtrl, limbPVCtrl):
         """
@@ -54,20 +72,39 @@ class ExtremityComponent(basecomponent.BaseComponent):
         :rtype: None
         """
 
-        limbPVSpaceSwitch = self.scene(limbPVCtrl.userProperties['spaceSwitch'])
-        index = limbPVSpaceSwitch.addTarget(extremityCtrl)
-        attributeName = f'transformSpaceW{index}'
+        # Check if space switch target already exists
+        #
+        spaceSwitch = self.scene(limbPVCtrl.userProperties['spaceSwitch'])
+        targets = spaceSwitch.targets()
+        targetName = extremityCtrl.name()
 
-        limbPVCtrl.addAttr(
-            longName=attributeName,
-            niceName=f'Transform Space ({self.componentName})',
-            attributeType='float',
-            min=0.0,
-            max=1.0,
-            keyable=True
-        )
+        found = [target for target in targets if target.name() == targetName]
+        exists = len(found) == 1
 
-        limbPVSpaceSwitch.connectPlugs(limbPVCtrl[attributeName], f'target[{index}].targetWeight')
+        if exists:
+
+            # Reconnect target to space switch
+            #
+            target = found[0]
+            extremityCtrl.connectPlugs(f'worldMatrix[{extremityCtrl.instanceNumber()}]', spaceSwitch[f'target[{target.index}].targetMatrix'], force=True)
+
+        else:
+
+            # Add target to space switch
+            #
+            index = spaceSwitch.addTarget(extremityCtrl)
+            attributeName = f'transformSpaceW{index}'
+
+            limbPVCtrl.addAttr(
+                longName=attributeName,
+                niceName=f'Transform Space ({self.componentName})',
+                attributeType='float',
+                min=0.0,
+                max=1.0,
+                keyable=True
+            )
+
+            spaceSwitch.connectPlugs(limbPVCtrl[attributeName], f'target[{index}].targetWeight')
 
     def overrideLimbHandle(self, extremityCtrl, inHandleCtrl):
         """
@@ -78,13 +115,32 @@ class ExtremityComponent(basecomponent.BaseComponent):
         :rtype: None
         """
 
-        inHandleSpaceSwitch = self.scene(inHandleCtrl.userProperties['spaceSwitch'])
-        insetNegate = self.scene(inHandleCtrl.userProperties['negate'])
+        # Check if space switch target already exists
+        #
+        negate = self.scene(inHandleCtrl.userProperties['negate'])
 
-        index = inHandleSpaceSwitch.addTarget(extremityCtrl, maintainOffset=False)
-        inHandleSpaceSwitch.setAttr(f'target[{index}]', {'targetWeight': (0.0, 0.0, 0.0)})
-        inHandleSpaceSwitch.connectPlugs(inHandleCtrl['localOrGlobal'], f'target[{index}].targetWeight')
-        inHandleSpaceSwitch.connectPlugs(insetNegate['outDistance'], f'target[{index}].targetOffsetTranslateX')
+        spaceSwitch = self.scene(inHandleCtrl.userProperties['spaceSwitch'])
+        targets = spaceSwitch.targets()
+        targetName = extremityCtrl.name()
+
+        found = [target for target in targets if target.name() == targetName]
+        exists = len(found) == 1
+
+        index = None
+
+        if exists:
+
+            index = found[0].index
+
+        else:
+
+            index = spaceSwitch.addTarget(extremityCtrl, maintainOffset=False)
+
+        # Update space switch
+        #
+        spaceSwitch.setAttr(f'target[{index}]', {'targetWeight': (0.0, 0.0, 0.0)})
+        spaceSwitch.connectPlugs(inHandleCtrl['localOrGlobal'], f'target[{index}].targetWeight', force=True)
+        spaceSwitch.connectPlugs(negate['outDistance'], f'target[{index}].targetOffsetTranslateX', force=True)
 
     def overrideLimbTwist(self, extremityCtrl, twistSolver, offsetMatrix=om.MMatrix.kIdentity):
         """
@@ -99,21 +155,20 @@ class ExtremityComponent(basecomponent.BaseComponent):
         twistSolver.endOffsetMatrix = offsetMatrix
         twistSolver.connectPlugs(extremityCtrl[f'worldMatrix[{extremityCtrl.instanceNumber()}]'], 'endMatrix', force=True)
 
-    def overrideLimbRemapper(self, extremityCtrl, scaleRemapper):
+    def overrideLimbRemapper(self, limbIKCtrl, extremityCtrl, scaleRemapper):
         """
         Overrides the end-value on the supplied scale remapper.
 
+        :type limbIKCtrl: mpynode.MPyNode
         :type extremityCtrl: mpynode.MPyNode
         :type scaleRemapper: mpynode.MPyNode
         :rtype: None
         """
 
-        limbTipCtrl = self.scene(scaleRemapper['outputMaxX'].source().node())
-
         multMatrixName = self.formatName(subname='Scale', type='multMatrix')
         multMatrix = self.scene.createNode('multMatrix', name=multMatrixName)
         multMatrix.connectPlugs(extremityCtrl['worldMatrix[0]'], 'matrixIn[0]')
-        multMatrix.connectPlugs(limbTipCtrl['parentInverseMatrix[0]'], 'matrixIn[1]')
+        multMatrix.connectPlugs(limbIKCtrl['parentInverseMatrix[0]'], 'matrixIn[1]')
 
         decomposeMatrixName = self.formatName(subname='Scale', type='decomposeMatrix')
         decomposeMatrix = self.scene.createNode('decomposeMatrix', name=decomposeMatrixName)
